ec547b1f8eb9fa331597021642f0af7e124a67e7
Crowdbotics <team@crowdbotics.com>
2023-07-10 17:02:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "finance-ecole-42068",
+  url: "https://finance-ecole-42068.botics.co",
+  api: "https://finance-ecole-42068.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
