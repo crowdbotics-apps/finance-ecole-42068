b3724054a3d4daf8768381177a0039c4e7f3e8eb
Crowdbotics <engineering@crowdbotics.com>
2023-07-11 01:07:56 +0000

    Update store.

--- store/financeecoleAPI/api.js
+++ store/financeecoleAPI/api.js
@@ -1,72 +1,58 @@
-import axios from "axios";
+import axios from "axios"
 const financeecoleAPI = axios.create({
   baseURL: "https://finance-ecole-42068.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return financeecoleAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return financeecoleAPI.post(`/api/v1/login/`, payload.data);
+  return financeecoleAPI.post(`/api/v1/login/`, payload.data)
 }
-
 function api_v1_signup_create(payload) {
-  return financeecoleAPI.post(`/api/v1/signup/`, payload.data);
+  return financeecoleAPI.post(`/api/v1/signup/`, payload.data)
 }
-
 function rest_auth_login_create(payload) {
-  return financeecoleAPI.post(`/rest-auth/login/`, payload.data);
+  return financeecoleAPI.post(`/rest-auth/login/`, payload.data)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return financeecoleAPI.get(`/rest-auth/logout/`);
+  return financeecoleAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return financeecoleAPI.post(`/rest-auth/logout/`);
+  return financeecoleAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return financeecoleAPI.post(`/rest-auth/password/change/`, payload.data);
+  return financeecoleAPI.post(`/rest-auth/password/change/`, payload.data)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return financeecoleAPI.post(`/rest-auth/password/reset/`, payload.data);
+  return financeecoleAPI.post(`/rest-auth/password/reset/`, payload.data)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return financeecoleAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
+  return financeecoleAPI.post(
+    `/rest-auth/password/reset/confirm/`,
+    payload.data
+  )
 }
-
 function rest_auth_registration_create(payload) {
-  return financeecoleAPI.post(`/rest-auth/registration/`, payload.data);
+  return financeecoleAPI.post(`/rest-auth/registration/`, payload.data)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return financeecoleAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
+  return financeecoleAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    payload.data
+  )
 }
-
 function rest_auth_user_retrieve(payload) {
-  return financeecoleAPI.get(`/rest-auth/user/`);
+  return financeecoleAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return financeecoleAPI.put(`/rest-auth/user/`, payload.data);
+  return financeecoleAPI.put(`/rest-auth/user/`, payload.data)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return financeecoleAPI.patch(`/rest-auth/user/`, payload.data);
+  return financeecoleAPI.patch(`/rest-auth/user/`, payload.data)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -82,4 +68,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
